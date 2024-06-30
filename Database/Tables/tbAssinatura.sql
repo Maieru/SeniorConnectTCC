992f3ddf6aa8ad62800daa7d5dd29010b0f4f4aa
Carolina <83421704+calmei43@users.noreply.github.com>
2024-06-30 16:45:09 -0300

    Update tbAssinatura.sql

--- Database/Tables/tbAssinatura.sql
+++ Database/Tables/tbAssinatura.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[tbAssinatura]
 (
-	[AssinaturaId]			INT			    NOT NULL	PRIMARY KEY	IDENTITY (1, 1),
+	[AssinaturaId]			INT	        NOT NULL	PRIMARY KEY	IDENTITY (1, 1),
 	[Descricao]	          	VARCHAR(200)	NOT NULL
 )
