a08fda93e4b11f8f01076e0335c4824c7f87e505
kauan <kauanmb@hotmail.com>
2024-08-24 21:35:57 -0300

    Adição navegação e telas iniciais Mobile

--- Sources/Software/SeniorConnect.MobileApp/App.js
+++ Sources/Software/SeniorConnect.MobileApp/App.js
@@ -1,61 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import styles from './styles.js';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MedicineScreen from './medicine.js';
+import HomeScreen from './home.js';
+import StatusScreen from './status.js';
+import ReportsScreen from './reports.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.headerImageContainer}>
-        <TouchableOpacity>
-          <Image
-              source={require('./assets/Footer/arrow.png')}
-              style={styles.headerImage}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.headerItem}>
-          <Text style={styles.headerText}> Home/Index</Text>
-        </View>
-
-      </View>
-      <View style={styles.content}>
-        
-      </View>
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.footerItem}>
-          <View style={styles.iconContainer}>
-            <Image
-              source={require('./assets/Footer/home.png')}
-              style={styles.footerImage}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.footerItem}>
-          <View style={styles.iconContainer}>
-            <Image
-              source={require('./assets/Footer/medicine.png')}
-              style={styles.footerImage}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.footerItem}>
-          <View style={styles.iconContainer}>
-            <Image
-              source={require('./assets/Footer/agenda.png')}
-              style={styles.footerImage}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.footerItem}>
-          <View style={styles.iconContainer}>
-            <Image
-              source={require('./assets/Footer/chart.png')}
-              style={styles.footerImage}
-            />
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="Medicine" component={MedicineScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="Status" component={StatusScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="Reports" component={ReportsScreen} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
---
+++ Sources/Software/SeniorConnect.MobileApp/reports.js
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import styles from './styles.js';
+
+export default function ReportsScreen({ navigation }) {
+return(
+<View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.headerImageContainer}>
+            <TouchableOpacity>
+              <Image
+                source={require('./assets/Footer/arrow.png')}
+                style={styles.headerImage} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.headerItem}>
+            <Text style={styles.headerText}> Reports</Text>
+          </View>
+
+        </View>
+        <View style={styles.content}>
+
+        </View>
+        <View style={styles.footer}>
+        <TouchableOpacity 
+          style={styles.footerItem}
+          onPress={() => navigation.navigate('Home') }>
+            <View style={styles.iconContainer}>
+              <Image
+                source={require('./assets/Footer/home.png')}
+                style={styles.footerImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.footerItem} 
+            onPress={() => navigation.navigate('Medicine') }>
+            <View style={styles.iconContainer}>
+              <Image
+                source={require('./assets/Footer/medicine.png')}
+                style={styles.footerImage}
+                />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.footerItem}
+            onPress={() => navigation.navigate('Status') }>
+            <View style={styles.iconContainer}>
+              <Image
+                source={require('./assets/Footer/agenda.png')}
+                style={styles.footerImage} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.footerItem}
+            onPress={() => navigation.navigate('Reports') }>
+            <View style={styles.iconContainer}>
+              <Image
+                source={require('./assets/Footer/chart.png')}
+                style={styles.footerImage} />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+)}
