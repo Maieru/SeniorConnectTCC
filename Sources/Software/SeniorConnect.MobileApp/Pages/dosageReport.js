5dcbbae957e8a3e7acafb5117e96e18119f4201f
kauan <kauanmb@hotmail.com>
2024-09-20 16:28:57 -0300

    Adição da lógica para a tela de DosageReport

--- Sources/Software/SeniorConnect.MobileApp/Pages/dosageReport.js
+++ Sources/Software/SeniorConnect.MobileApp/Pages/dosageReport.js
@@ -3,24 +3,80 @@ import { Text, View, ScrollView, Dimensions } from 'react-native';
 import { HeaderReturn, Footer } from '../Layout.js';
 import styles from '../styles.js';
 import stylesDosageReport from '../stylesDosageReport.js';
+import apiClient from '../services/apiService.js';
+import { useEffect, useState } from 'react';
 
 const { width, height } = Dimensions.get('window');
 
 export default function DosageReportScreen({ navigation }) {
 
-    const days = [
-        { day: "Segunda", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-        { day: "Terça", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-        { day: "Quarta", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-        { day: "Quinta", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-        { day: "Sexta", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-        { day: "Sábado", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-        { day: "Domingo", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
-    ];
+    const [report, setReport] = useState();
+    const [days, setDays] = useState([]);
+    // await apiClient.get("/v1/Report/GetWeeklySchedulesReport?subscriptionId=" + apiClient.getSubscription());
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await constroiData();
+            const daysData = await buildDaysData(data);
+            setDays(daysData);
+        }
+        fetchData();
+
+        return () => { }; //É feio, é estranho.... mas funciona. Juro que tirando isso morre
+    }, []
+    )
+
+    async function getMedicinesByDay(dado, dayOfWeek) {
+        if (!dado || !dado.length) return [];
+
+        const daySchedule = dado.find(day => day.dayOfWeek === dayOfWeek);
+
+        if (daySchedule) {
+            return daySchedule.scheduledMedicines;
+        } else {
+            return [];
+        }
+    }
+
+    async function constroiData() {
+        const response = await apiClient.get("/v1/Report/GetWeeklySchedulesReport?subscriptionId=" + apiClient.getSubscription());
+        setReport(response.data);
+        
+        return response.data;
+    }
+
+    async function buildDaysData(reportData) {
+        const daysOfWeek = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+        const daysData = [];
+
+        for (let i = 0; i < 7; i++) {
+            const medicines = await getMedicinesByDay(reportData, i);
+            const medicationsFormatted = medicines.map(med => ({
+                name: med.medicineName,
+                dosage: med.medicineTimes.map(time => `${time.hour}:${time.minute < 10 ? '0' + time.minute : time.minute}`).join(", ")
+            }));
+
+            daysData.push({
+                day: daysOfWeek[i],
+                medications: medicationsFormatted
+            });
+        }
+
+        return daysData;
+    }
+    {/**    const days = [
+    { day: "Segunda", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+    { day: "Terça", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+    { day: "Quarta", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+    { day: "Quinta", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+    { day: "Sexta", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+    { day: "Sábado", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+    { day: "Domingo", medications: [{ name: "[Medicamento 1]", dosage: "HH:MM" }, { name: "[Medicamento 2]", dosage: "HH:MM" }] },
+];*/}
+
 
     return (
         <View style={styles.containerMenu}>
-            {/* HeaderReturn component with back navigation to Reports screen */}
             <HeaderReturn title="Voltar" returnPage="Reports" navigation={navigation} />
             <View style={styles.content}>
                 <ScrollView
