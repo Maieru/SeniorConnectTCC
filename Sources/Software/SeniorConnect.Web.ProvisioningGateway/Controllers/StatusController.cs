3bc56111c35af4095e56ff463fc0cac9ca462dee
Maieru <soljoaomaieru@hotmail.com>
2024-09-29 12:38:24 -0300

    Adição de actions para recuperar status dos sites de API e de provisionamento

---
+++ Sources/Software/SeniorConnect.Web.ProvisioningGateway/Controllers/StatusController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SeniorConnect.Bussiness.Services;
+
+namespace SeniorConnect.Web.ApiServer.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly LogService _logService;
+        
+        public StatusController(LogService logService)
+        {
+            _logService = logService;
+        }
+
+        [HttpGet("GetStatus")]
+        [AllowAnonymous]
+        public IActionResult GetStatus() => Ok("Everything is fine!");
+    }
+}
