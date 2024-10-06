57c5cf5853b108f3bb3031de4094e8782b12fe34
Maieru <soljoaomaieru@hotmail.com>
2024-10-06 12:29:51 -0300

    Correções nos iot_config

--- Sources/Firmware/iot_config.h
+++ Sources/Firmware/iot_config.h
@@ -1,4 +1,3 @@
 // Defines
-#define IOT_CONFIG_IOTHUB_FQDN "testecourse-iot.azure-devices.net"
-#define IOT_CONFIG_DEVICE_KEY "67hFcii0UnhwjKOm1pxyJiP6WdtuWWVlU75wGBRghi8="
 
+#define IOT_CONFIG_IOTHUB_FQDN "seniorconnectdeviothub.azure-devices.net"
--- Sources/Firmware/iot_config_template.h
+++ Sources/Firmware/iot_config_template.h
@@ -1,4 +1,2 @@
 // Defines
 #define IOT_CONFIG_IOTHUB_FQDN "nice-try.net"
-#define IOT_CONFIG_DEVICE_ID "nice-try"
-#define IOT_CONFIG_DEVICE_KEY "nice-try765="
