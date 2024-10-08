d2e49e20270be2f4e078a002e38900491f1623b6
Maieru <soljoaomaieru@hotmail.com>
2024-10-07 21:08:54 -0300

    Correção ao editar um agendamento, fazendo com que a navegação ocorra corretamente

--- Sources/Software/SeniorConnect.MobileApp/Pages/newScheduling.js
+++ Sources/Software/SeniorConnect.MobileApp/Pages/newScheduling.js
@@ -104,7 +104,7 @@ export default function HorariosMedicineScreen({ navigation, route }) {
                 [
                     {
                         text: "OK",
-                        onPress: () => navigation.navigate('NewMedicine')
+                        onPress: () => navigation.navigate('Novo Medicamento')
                     }
                 ]
             );
