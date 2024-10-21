8e0dc4d762cbb2bb5065ea379b1a77bc9c221705
Maieru <soljoaomaieru@hotmail.com>
2024-10-21 20:35:10 -0300

    Correção na API de recuperar medicamentos não administrador

---
+++ Sources/Software/SeniorConnect.Domain/TOs/Medicine/UnadministeredSchedulingsTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorConnect.Domain.TOs.Medicine
+{
+    public class UnadministeredSchedulingsTO
+    {
+        public int SchedulingId { get; set; }
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public string DaysOfWeek { get; set; }
+    }
+}
