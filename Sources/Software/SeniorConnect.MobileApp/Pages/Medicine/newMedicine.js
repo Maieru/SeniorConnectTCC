1f32dad5c2c18838d3ba7cb91caf9f96165b5b50
CAROLINA DA SILVA ALMEIDA <calmeid42@gmail.com>
2024-11-08 13:22:42 -0300

    add scrollview newMedicine

--- Sources/Software/SeniorConnect.MobileApp/Pages/Medicine/newMedicine.js
+++ Sources/Software/SeniorConnect.MobileApp/Pages/Medicine/newMedicine.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView, TextInput, Alert } from 'react-native';
-import { HeaderReturn, Footer} from '../../Layout.js';
+import { HeaderReturn, Footer } from '../../Layout.js';
 import React, { useState, useEffect } from 'react';
 import apiClient from '../../services/apiService.js';
 import { useFocusEffect } from '@react-navigation/native';
@@ -21,8 +21,6 @@ export default function MedicineScreen({ navigation, route }) {
             console.log("Por algum motivo, essa api demora, mas não da problema...");
         }
     }
-
-    
 
     useFocusEffect(
         React.useCallback(() => {
@@ -92,55 +90,55 @@ export default function MedicineScreen({ navigation, route }) {
 
     function HorariosMedicine({ horario, diasSemana, id, medicamentoId, navigation }) {
         return (
-          <View style={styles.scheduleContainer}>
-            <View style={styles.horarioContainer}>
-              <Text style={styles.horarioText}>{horario}</Text>
+            <View style={styles.scheduleContainer}>
+                <View style={styles.horarioContainer}>
+                    <Text style={styles.horarioText}>{horario}</Text>
+                </View>
+                <View style={styles.diasContainer}>
+                    <View style={styles.diasSemanaContainer}>
+                        {diasSemana.split('').map((dia, index) => (
+                            <Text key={index} style={styles.diaSemana}>{dia}</Text>
+                        ))}
+                    </View>
+                </View>
+                <View style={styles.actionsContainer}>
+                    <TouchableOpacity
+                        style={styles.actionButton}
+                        onPress={() => navigation.navigate('Novo Horario', { medicine: medicamentoId, scheduling: id })}
+                    >
+                        <Image
+                            source={require('../../assets/edit.png')}
+                            style={styles.actionIcon}
+                        />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.actionButton}
+                        onPress={async () => await deletaSchedule(id, navigation)}
+                    >
+                        <Image
+                            source={require('../../assets/delete.png')}
+                            style={styles.actionIcon}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.diasContainer}>
-              <View style={styles.diasSemanaContainer}>
-                {diasSemana.split('').map((dia, index) => (
-                  <Text key={index} style={styles.diaSemana}>{dia}</Text>
-                ))}
-              </View>
-            </View>
-            <View style={styles.actionsContainer}>
-              <TouchableOpacity
-                style={styles.actionButton}
-                onPress={() => navigation.navigate('Novo Horario', { medicine: medicamentoId, scheduling: id })}
-              >
-                <Image
-                  source={require('../../assets/edit.png')}
-                  style={styles.actionIcon}
-                />
-              </TouchableOpacity>
-      
-              <TouchableOpacity
-                style={styles.actionButton}
-                onPress={async () => await deletaSchedule(id, navigation)}
-              >
-                <Image
-                  source={require('../../assets/delete.png')}
-                  style={styles.actionIcon}
-                />
-              </TouchableOpacity>
-            </View>
-          </View>
         )
-      }
-      
-      async function deletaSchedule(id) {
+    }
+
+    async function deletaSchedule(id) {
         try {
             setHorarios((prevHorarios) => prevHorarios.filter((horario) => horario.id !== id));
-            
+
             await apiClient.delete("/v1/Scheduling/Delete?schedulingId=" + id);
-            
+
             Alert.alert(
                 "Feito!",
                 "Horário deletado como solicitado!",
                 [
                     {
                         text: "OK",
-                        onPress: () => listaHorarios() 
+                        onPress: () => listaHorarios()
                     }
                 ]
             );
@@ -180,50 +178,52 @@ export default function MedicineScreen({ navigation, route }) {
     }
 
     return (
-        <View style={styles.containerMenu}>
-            <View style={styles.content}>
-                <View style={styles.container}>
-                    <View style={styles.sectionContainer}>
-                        <TextInput
-                            style={styles.cadastroInput}
-                            placeholder="Nome do Medicamento"
-                            placeholderTextColor="#888"
-                            value={medicamentoNome}
-                            onChangeText={setMedicamentoNome}
-                        />
-                        <TouchableOpacity
-                            onPress={() => salvaMedicamento()}
-                            style={styles.cadastroButton}>
-                            <Text style={styles.basicButtonText}>Salvar</Text>
-                        </TouchableOpacity>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <View style={styles.containerMenu}>
+                <View style={styles.content}>
+                    <View style={styles.container}>
+                        <View style={styles.sectionContainer}>
+                            <TextInput
+                                style={styles.cadastroInput}
+                                placeholder="Nome do Medicamento"
+                                placeholderTextColor="#888"
+                                value={medicamentoNome}
+                                onChangeText={setMedicamentoNome}
+                            />
+                            <TouchableOpacity
+                                onPress={() => salvaMedicamento()}
+                                style={styles.cadastroButton}>
+                                <Text style={styles.basicButtonText}>Salvar</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
+                    {medicamentoId != null && (
+                        <View style={styles.container}>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.basicLabel}>Horários</Text>
+                                <TouchableOpacity
+                                    onPress={() => navigation.navigate('Novo Horario', { medicine: medicamentoId })}
+                                    style={styles.newMedicineButton}>
+                                    <Text style={styles.basicButtonText}>Novo Horário/Dosagem</Text>
+                                </TouchableOpacity>
+                                <ScrollView style={styles.basicScroll}>
+                                    {horarios.map(horario => (
+                                        <HorariosMedicine
+                                            key={horario.id}
+                                            horario={`${horario.hour}:${horario.minute < 10 ? '0' + horario.minute : horario.minute}`}
+                                            diasSemana={formatarDiasSemana(horario.daysOfWeek)}
+                                            id={horario.id}
+                                            medicamentoId={medicamentoId}
+                                            navigation={navigation}
+                                        />
+                                    ))}
+                                </ScrollView>
+                            </View>
+                        </View>
+                    )}
                 </View>
-                {medicamentoId != null && (
-                <View style={styles.container}>
-                    <View style={styles.sectionContainer}>
-                        <Text style={styles.basicLabel}>Horários</Text>
-                        <TouchableOpacity
-                            onPress={() => navigation.navigate('Novo Horario', { medicine: medicamentoId })}
-                            style={styles.newMedicineButton}>
-                            <Text style={styles.basicButtonText}>Novo Horário/Dosagem</Text>
-                        </TouchableOpacity>
-                        <ScrollView style={styles.basicScroll}>
-                            {horarios.map(horario => (
-                                <HorariosMedicine
-                                    key={horario.id}
-                                    horario={`${horario.hour}:${horario.minute < 10 ? '0' + horario.minute : horario.minute}`}
-                                    diasSemana={formatarDiasSemana(horario.daysOfWeek)}
-                                    id={horario.id}
-                                    medicamentoId={medicamentoId}
-                                    navigation={navigation}
-                                />
-                            ))}
-                        </ScrollView>
-                    </View>
-                </View>
-                )}
+                <Footer navigation={navigation} />
             </View>
-            <Footer navigation={navigation} />
-        </View>
+        </ScrollView>
     )
 }
